3c79fe3281f17308654e225baf8ce652c35df27c
pmorris2012 <pmorris2012@fau.edu>
2021-08-25 01:15:21 -0400

    test configs

--- histograms/build_configs.py
+++ histograms/build_configs.py
@@ -1,10 +1,16 @@
 import json
+from pathlib import Path
 import numpy as np
 import typer
 
 
 def get_frame_strides(samples, frames):
-    return np.round(np.linspace(0, samples, frames + 1))[1:-1].astype(np.int32)
+    strides = np.linspace(0, samples, frames + 1)
+    strides = np.round(strides)[1:-1].astype(np.int32)
+    strides = list(map(int, strides))
+    strides_start = [0] * frames + strides
+    strides_end = strides + [samples] * frames
+    return strides_start, strides_end
 
 def main(
     name: str, 
@@ -13,28 +19,44 @@ def main(
     samples: int = 100,
     n_start: int = 2,
     n_end: int = 20,
-    bins_2d: int = 2160,
     bins_1d: int = 10000,
-    blur: int = 9
+    bins_2d: int = 2160,
+    blur: int = 9,
+    configs_dir: str = "configs"
     ):
-    frame_strides = get_frame_strides(samples, frames)
+    strides_start, strides_end = get_frame_strides(samples, frames)
 
     count = jobs * samples
-    lin_steps = np.linspace(0, jobs, count - 1)
-    geom_steps = np.geomspace(n_start - 1, n_end - lin_steps[-1] - 2, count - 1)
+    linsteps = np.linspace(0, jobs, count - 1)
+    geomsteps = np.geomspace(n_start - 1, n_end - linsteps[-1] - 1, count - 1)
     base_steps = np.full(count, 1.0)
-    base_steps[0] += 1
-    base_steps[1:] += lin_steps + geom_steps
-    base_steps = np.round(base_steps).astype(np.int32)
+    base_steps[0] += n_start - 1
+    base_steps[1:] += linsteps + geomsteps
+    sample_steps = np.round(base_steps).astype(np.int32).reshape(jobs, samples)
+
+    frame_dims = np.zeros((jobs, frames * 2 - 1))
+    for i, (start, end) in enumerate(zip(strides_start, strides_end)):
+        frame_dims[:,i] = sample_steps[:,start:end].sum(axis=-1)
 
     for job_idx in range(jobs):
         job = {
             "name": name,
             "index": job_idx,
-            "frames": frames
+            "frames": frames,
+            "sample_dims": list(map(int, sample_steps[job_idx])),
+            "partial_frame_dims": list(map(int, frame_dims[job_idx])),
+            "strides_start": strides_start,
+            "strides_end": strides_end,
+            "bins_1d": bins_1d,
+            "bins_2d": bins_2d,
+            "blur": blur
         }
-    #json.dump()
-    typer.echo(f"{base_steps}")
+
+        out_path = Path(configs_dir, f"{name}-{job_idx}.json")
+        with open(out_path, "w") as f:
+            json.dump(job, f, sort_keys=True, indent=4)
+
+    typer.echo(f"wrote config to {out_path}")
 
 
 if __name__ == "__main__":
