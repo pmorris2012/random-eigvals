d4662a48aefb15c0a53609b04ee90e91089b8416
Paul Morris <pmorris2012@fau.edu>
2021-09-14 12:49:44 -0400

    script for single dimension

--- generate_eigenvalues.py
+++ generate_eigenvalues.py
@@ -1,31 +1,39 @@
 import numpy as np
 import typer
 import matplotlib.pyplot as plt
+
 from pathlib import Path
 
 
+def random_matrix(n):
+    return np.random.randn(n, n)
+
+
+def generate(n, iters):
+    vecs = random_matrix(n)
+    eigenvalues = np.zeros((iters, n), dtype=np.complex64)
+    for i_idx in range(iters):
+        vals, vecs = np.linalg.eig(vecs)
+        eigenvalues[i_idx] = vals
+    return eigenvalues
 
 
 def main(
-    samples = 1, 
-    n = 100, 
-    iterations = 1002,
+    samples: int = 1,
+    start_index: int = 0,
+    dimension: int = 100,
+    iterations: int = 1002,
     arrays_dir: Path = "arrays/"
     ):
-    arrays_dir.mkdir(parent, )=True, exist_ok=True)
+    arrays_dir.mkdir(parent=True, exist_ok=True)
+
+    for s_idx in range(start_index, start_index + samples):
+        eigenvalues = generate(n, iters)
+
+        filename = F"n_{dimension}_i_{iterations}_s_{s_idx}.npy"
+        save_path = Path(arrays_dir, filename)
+        np.save(save_path, eigenvalues)
 
 
-mies = np.zeros((n, n))
-for i in tqdm.tqdm(range(1, n + 1, 10)):
-    for j in tqdm.tqdm(range(samples), desc=str(i)):
-        vecs = np.random.randn(i, i)
-        for _ in range(skip_iters):
-            vals, vecs = np.linalg.eig(vecs)
-        for k in range(iters):
-            vals, vecs = np.linalg.eig(vecs)
-            mies[i-1,:i] += np.abs(vals)
-mies /= iters * samples
-for i in range(1, n):
-    plt.plot(np.arange(1, n-i+1), mies[:n-i,i])
-plt.yscale('symlog')
-plt.show()
+if __name__ == "__main__":
+    typer.run(main)
