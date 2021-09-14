d50312454e422dedccd05b8d3c9013f0d262ccdc
Paul Morris <pmorris2012@fau.edu>
2021-09-14 16:04:44 -0400

    timer for logs

--- generate_eigenvalues.py
+++ generate_eigenvalues.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from pathlib import Path
 import uuid
+import time
 
 
 def random_string():
@@ -32,7 +33,10 @@ def main(
     arrays_dir.mkdir(parents=True, exist_ok=True)
 
     for s_idx in range(samples):
+        start_time = time.time()
         eigenvalues = generate(dimension, iterations)
+        duration = time.time() - start_time
+        print(F"Sample {s_idx}, {duration} seconds")
 
         filename = F"n_{dimension}_i_{iterations}_{random_string()}.npy"
         save_path = Path(arrays_dir, filename)
